b812f372015595415a598c6f8036361531783352
Yusuf Morsi <ymorsi7@gmail.com>
2023-05-23 07:33:30 -0500

    Update 3dMapping.py

--- 3dMapping.py
+++ 3dMapping.py
@@ -109,7 +109,7 @@ class CoordinateFrame:
         self.frame.transform(prevToCurrent)
         self.camToWorld = camToWorld
 
-lass Open3DVisualization:
+class Open3DVisualization:
     def __init__(self, voxelSize, cameraManual, cameraSmooth, colorOnly, trajectory=False):
         self.shouldClose = False
         self.cameraFrame = CoordinateFrame()
@@ -215,6 +215,116 @@ lass Open3DVisualization:
         pc = self.pointClouds[keyFrameId]
         pc.status = Status.REMOVED
 
+def parseArgs():
+    import argparse
+    p = argparse.ArgumentParser(__doc__)
+    p.add_argument("--dataFolder", help="Instead of running live mapping session, replay session from this folder")
+    p.add_argument('--file', type=argparse.FileType('rb'), help='Read data from file or pipe, using this with mapping_visu C++ example', default=None)
+    p.add_argument("--recordingFolder", help="Record live mapping session for replay")
+    p.add_argument("--outputFolder", help="Folder where to save the captured point clouds")
+    p.add_argument("--voxel", help="Voxel size (m) for downsampling point clouds")
+    p.add_argument("--manual", help="Control Open3D camera manually", action="store_true")
+    p.add_argument("--smooth", help="Apply some smoothing to 3rd person camera movement", action="store_true")
+    p.add_argument("--color", help="Filter points without color", action="store_true")
+    p.add_argument("--use_rgb", help="Use OAK-D RGB camera", action="store_true")
+    p.add_argument("--trajectory", help="Draw camera trajectory", action="store_true")
+    p.add_argument('--ir_dot_brightness', help='OAK-D Pro (W) IR laser projector brightness (mA), 0 - 1200', type=float, default=0)
+    p.add_argument('--no_feature_tracker', help='Disable on-device feature tracking and depth map', action="store_true")
+    p.add_argument("--useRectification", help="--dataFolder option can also be used with some non-OAK-D recordings, but this parameter must be set if the videos inputs are not rectified.", action="store_true")
+    return p.parse_args()
 
+if __name__ == '__main__':
+    args = parseArgs()
+    if args.outputFolder:
+        os.makedirs(args.outputFolder)
 
+    configInternal = {
+        "computeStereoPointCloud": "true",
+        "pointCloudNormalsEnabled": "true",
+        "computeDenseStereoDepth": "true",
+    }
+    if args.dataFolder and args.useRectification:
+        configInternal["useRectification"] = "true"
+    else:
+        configInternal["alreadyRectified"] = "true"
+
+    voxelSize = 0 if args.voxel is None else float(args.voxel)
+    visu3D = Open3DVisualization(voxelSize, args.manual, args.smooth, args.color, args.trajectory)
+
+    def onVioOutput(vioOutput):
+        cameraPose = vioOutput.getCameraPose(0)
+        camToWorld = cameraPose.getCameraToWorldMatrix()
+        cameraPoses.append(camToWorld)
+
+    def onMappingOutput(output):
+        for frameId in output.updatedKeyFrames:
+            keyFrame = output.map.keyFrames.get(frameId)
+
+            # Remove deleted key frames from visualization
+            if not keyFrame:
+                if visu3D.containsKeyFrame(frameId): visu3D.removeKeyFrame(frameId)
+                continue
+
+            # Check that point cloud exists
+            if not keyFrame.pointCloud: continue
+
+            # Render key frame point clouds
+            if visu3D.containsKeyFrame(frameId):
+                # Existing key frame
+                visu3D.updateKeyFrame(frameId, keyFrame, cameraPoses)
+            else:
+                # New key frame
+                visu3D.addKeyFrame(frameId, keyFrame, cameraPoses)
+
+        if output.finalMap:
+            print("Final map ready!")
+
+    if args.file:
+        print("Starting reading input from file or pipe")
+        def inputStreamLoop():
+            vio_source = input_stream_reader(args.file)
+            for vio_out in vio_source:
+                if 'cameraPoses' in vio_out: onVioOutput(MockVioOutput(vio_out))
+                else: onMappingOutput(MockMapperOutput(vio_out))
+        thread = threading.Thread(target=inputStreamLoop)
+        thread.start()
+        visu3D.run(cameraPoses)
+        thread.join()
+    elif args.dataFolder:
+        print("Starting replay")
+        replay = spectacularAI.Replay(args.dataFolder, onMappingOutput, configuration=configInternal)
+        replay.setOutputCallback(onVioOutput)
+        replay.startReplay()
+        visu3D.run(cameraPoses)
+        replay.close()
+    else:
+        def captureLoop():
+            print("Starting OAK-D device")
+            pipeline = depthai.Pipeline()
+            config = spectacularAI.depthai.Configuration()
+            config.useFeatureTracker = not args.no_feature_tracker
+            if args.recordingFolder:
+                config.recordingFolder = args.recordingFolder
+            config.useColor = args.use_rgb
+            config.internalParameters = configInternal
+            vioPipeline = spectacularAI.depthai.Pipeline(pipeline, config, onMappingOutput)
+
+            with depthai.Device(pipeline) as device, \
+                vioPipeline.startSession(device) as vio_session:
+                if args.ir_dot_brightness > 0:
+                    device.setIrLaserDotProjectorBrightness(args.ir_dot_brightness)
+                while not visu3D.shouldClose:
+                    onVioOutput(vio_session.waitForOutput())
+
+        thread = threading.Thread(target=captureLoop)
+        thread.start()
+        visu3D.run(cameraPoses)
+        thread.join()
+
+    if args.outputFolder:
+        print("Saving point clouds to {0}".format(args.outputFolder))
+        for id in visu3D.pointClouds:
+            pc = visu3D.pointClouds[id]
+            filename = "{0}/{1}.ply".format(args.outputFolder, id)
+            o3d.io.write_point_cloud(filename, pc.cloud)
 
