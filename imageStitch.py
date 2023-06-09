9555b0ed154df7ca2b441160486d7a2582ad01a8
Yusuf Morsi <ymorsi7@gmail.com>
2023-06-08 20:14:53 -0700

    got the panoramic images to save in a folder

--- imageStitch.py
+++ imageStitch.py
@@ -1,6 +1,5 @@
 import os
 from PIL import Image
-# note: current python version is 3.7.5 
 
 def merge_images(file_paths):
     """
@@ -31,3 +30,28 @@ panoImages = [os.path.join(image_directory, filename) for filename in file_names
 
 merged_image = merge_images(panoImages)
 merged_image.show()
+
+# Create the 'Pano' directory if it doesn't exist
+output_directory = "Pano"
+os.makedirs(output_directory, exist_ok=True)
+
+# Find the highest existing count in the 'Pano' directory
+existing_files = os.listdir(output_directory)
+existing_counts = []
+for file_name in existing_files:
+    if file_name.endswith(".jpg"):
+        try:
+            count = int(file_name.split("_")[0])
+            existing_counts.append(count)
+        except ValueError:
+            pass
+
+highest_count = max(existing_counts) if existing_counts else 0
+
+# Increment the count and construct the output file name
+count = highest_count + 1
+output_filename = f"{count:03d}_merged_image.jpg"
+output_path = os.path.join(output_directory, output_filename)
+
+# Save the merged image
+merged_image.save(output_path)
