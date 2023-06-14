f6eab82aade3a9956c583d2e2dd669e9b5c4d752
colin-szeto <kolinszeto@gmail.com>
2023-06-14 16:16:37 -0700

    pushing martin code

---
+++ process.py
@@ -0,0 +1,13 @@
+import subprocess
+
+# Run the first script
+subprocess.run(['python', 'new.py'])
+
+# Run the second script
+subprocess.run(['python', 'c.py'])
+
+subprocess.run(['python', '/home/jetson/AutonomousPhotogrammetry/pvescOG/edit.py'])
+
+subprocess.run(['python', 'new.py'])
+
+subprocess.run(['python', 'c.py'])
